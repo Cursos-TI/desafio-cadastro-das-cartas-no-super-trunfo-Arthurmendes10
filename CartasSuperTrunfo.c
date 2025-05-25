f1068cdc7f1e9ddaf4d697c6fb207ff25688d496
Arthurmendes10 <arthurmendesdemouraoficial@gmail.com>
2025-05-25 19:02:18 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,7 +18,64 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-printf("Olá, Mundo!/n");
+
+    int codigocidade1;
+    int nome1;
+    int populacao1;
+    int area1;
+    int pib1;
+    int pontosturisticos1;
+
+    int codigocidade2;
+    int nome2;
+    int populacao2;
+    int area2;
+    int pib2;
+    int pontosturisticos2;
+
+    printf("Vamos cadastrar a primeira cara do super trunfo!");
+    
+    printf("Porfavor, digite o código da cidade da primeira carta:");
+    scanf("%s", codigocidade1);
+
+    printf("Porfavor, digite o nome da cidade da primeira carta:");
+    scanf("%s", nome1);
+
+    printf("Porfavor, digite o valor da população da cidade da primeira carta:");
+    scanf("%s", populacao1);
+
+    printf("Porfavor, digite o valor da área territorial da cidade da primeira carta:");
+    scanf("%s", area1);
+
+    printf("Porfavor, digite o valor do PIB da cidade da primeira carta:");
+    scanf("%s", pib1);
+
+    printf("Porfavor, digite o número de pontos turísticos da cidade da primeira carta:");
+    scanf("%s", pontosturisticos1);
+
+
+
+    printf("Vamos cadastrar a segunda cara do super trunfo!");
+    
+    printf("Porfavor, digite o código da cidade da segunda carta:");
+    scanf("%s", codigocidade2);
+
+    printf("Porfavor, digite o nome da cidade da segunda carta:");
+    scanf("%s", nome2);
+
+    printf("Porfavor, digite o valor da população da cidade da segunda carta:");
+    scanf("%s", populacao2);
+
+    printf("Porfavor, digite o valor da área territorial da cidade da segunda carta:");
+    scanf("%s", area2);
+
+    printf("Porfavor, digite o valor do PIB da cidade da segunda carta:");
+    scanf("%s", pib2);
+
+    printf("Porfavor, digite o número de pontos turísticos da cidade da segunda carta:");
+    scanf("%s", pontosturisticos2);
+
+    printf("Meus parabéns!, você cadastrou as cartas deste jogo de trunfo super interessante que utiliza apenas duas cartas, risos!"/n);
     
     return 0;
 }
